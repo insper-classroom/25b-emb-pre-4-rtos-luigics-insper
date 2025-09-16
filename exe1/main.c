9358254dede2196d34ce38be65ea4137a500ba41
Luigi <luigics@al.insper.edu.br>
2025-09-16 13:43:38 -0300

    exe1

--- exe1/main.c
+++ exe1/main.c
@@ -8,9 +8,7 @@
 
 
 const int LED_PIN_R = 4;
-const int BTN_PIN = 7;
 const int LED_PIN_G = 6;
-SemaphoreHandle_t xSemaphore_r;
 
 void led_1_task(void *p) {
   gpio_init(LED_PIN_R);
@@ -31,37 +29,19 @@ void led_2_task(void *p) {
 
   int delay = 250;
   while (true) {
-    if (xSemaphoreTake(xSemaphore_r,pdMS_TO_TICKS(500)) == pdTRUE){
-      gpio_put(LED_PIN_G, 1);
-      vTaskDelay(pdMS_TO_TICKS(delay));
-      gpio_put(LED_PIN_G, 0);
-      vTaskDelay(pdMS_TO_TICKS(delay));
-    }
+    gpio_put(LED_PIN_G, 1);
+    vTaskDelay(pdMS_TO_TICKS(delay));
+    gpio_put(LED_PIN_G, 0);
+    vTaskDelay(pdMS_TO_TICKS(delay));
   }
 }
 
-void btn_2_task(void *p) {
-  gpio_init(BTN_PIN);
-  gpio_set_dir(BTN_PIN, GPIO_IN);
-  gpio_pull_up(BTN_PIN);
-  while (true) {
-    if (!gpio_get(BTN_PIN)) {
-      while (!gpio_get(BTN_PIN)) {
-        vTaskDelay(pdMS_TO_TICKS(1));
-      }
-      xSemaphoreGive(xSemaphore_r);
-    }
-  }
-} 
-
 int main() {
   stdio_init_all();
-  xSemaphore_r = xSemaphoreCreateBinary();
   printf("Start RTOS \n");
 
   xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
   xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
-  xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);
   vTaskStartScheduler();
 
   while (true)
